917c81a8e2cedcdf274f3e788c869a0f85ee5275
Mehul Parmar <mehulparmar5028@gmail.com>
2024-04-22 11:30:34 +0530

    WordPattern

---
+++ WordPattern.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] arr = s.split(" ");
+
+        if(pattern.length() != arr.length) return false;
+
+        HashMap<Character,String> map = new HashMap<>();
+
+        for(int i=0;i<pattern.length();i++){
+            if(map.containsKey(pattern.charAt(i))){
+                if(!(map.get(pattern.charAt(i)).equals(arr[i]))) return false;
+            }
+            else if(map.containsValue(arr[i])) return false;
+            map.put(pattern.charAt(i),arr[i]);
+        }
+        return true;
+    }
+}
